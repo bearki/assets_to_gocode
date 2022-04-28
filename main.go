ac5b60b9c3342acd21a7982dc27cfce008e50c4c
bearki <bearki@126.com>
2022-04-28 14:49:48 +0800

    优化输出格式

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		Name:      "AssetsToGoCode",
 		Usage:     "Encode static resources into Golang code",
 		UsageText: execFileName + " command [command options]",
-		Version:   "v0.0.1",
+		Version:   "v0.0.2",
 
 		// 命令列表
 		Commands: []*cli.Command{
